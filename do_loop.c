482a411c94649f88068b298ad4b09266358be178
Mahanth-Maha <mahanthysree@gmail.com>
2020-11-29 19:09:31 +0530

    do while loop example - ( Timer )

---
+++ do_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int i=0,n;
+    printf("\n\t\tTimer\n\n");
+    printf("Enter time to count down in sec : ");
+    scanf("%d",&n);
+    do{
+        printf("%d\n",n--);
+        sleep(1);
+    }while(n!=0);
+    printf("\nTimeUp !\n")
+    return 0;
+}
+
+
